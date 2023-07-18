e076fa9d211048bec1c3813bee43f2fbfd125dc5
Minh Nghia DUONG <minhnghia.duong@amadeus.com>
2023-07-18 23:01:38 +0200

    list tags

--- Controllers/AdminTagsController.cs
+++ Controllers/AdminTagsController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using blog_app.Data;
+using blog_app.Models.Domain;
 using blog_app.Models.Views;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -13,12 +15,14 @@ namespace blog_app.Controllers
     public class AdminTagsController : Controller
     {
         private readonly ILogger<AdminTagsController> _logger;
+        private readonly BlogDbContext _dbContext;
 
-        public AdminTagsController(ILogger<AdminTagsController> logger)
+        public AdminTagsController(ILogger<AdminTagsController> logger, BlogDbContext dbContext)
         {
             _logger = logger;
+            _dbContext = dbContext;
         }
-
+        
         [HttpGet]
         public IActionResult Add()
         {
@@ -30,15 +34,29 @@ namespace blog_app.Controllers
         [ActionName("Add")]
         public IActionResult Post(AddTagRequest request)
         {
-            _logger.LogInformation(message: "saving tag");
-            string name = Request.Form["name"];
-            string displayName = Request.Form["displayName"];
+            Tag newTag = new Tag
+            {
+                Name = request.Name,
+                DisplayName = request.DisplayName
+            };
+
+            _dbContext.Tags.Add(newTag);
+            _dbContext.SaveChanges();
 
             // Render tha Add view
-            return View("Add");
+            return RedirectToAction("List");
         }
 
-        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [HttpGet]
+        [ActionName("List")]
+        public IActionResult List()
+        {   
+            var tags = _dbContext.Tags.ToList();
+
+            return View(tags);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View("Error!");
